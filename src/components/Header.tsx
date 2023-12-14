08fcd65a2fc8a11c428ebf0ea9695f6f9e9a9517
Nicolas Osorio <osorionicolas95@gmail.com>
2023-12-14 03:12:21 +0100

    improve header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,12 @@
 'use client'
 import Link from "next/link";
 import {
-  CreditCard,
-  Github,
-  LogIn,
-  LogOut,
-  Plus,
   Settings,
-  User,
-  UserPlus,
-  Users,
+  Plus,
+  Github,
+  LogOut,
+  LogIn,
+  ChevronDown,
 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
@@ -22,54 +19,69 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { signIn, signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 export default function Header() {
-    const { data: session } = useSession()
-    const router = useRouter()
-    return (
-        <header className="w-full items-center justify-between lg:flex bg-green-500 text-white">
-            <Link href="/">Moneytalks</Link>
-            {session ? (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost">{session?.user?.name}</Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                            <DropdownMenuItem asChild>
-                                <Link href="/account/settings">
-                                    <Settings className="mr-2 h-4 w-4" />
-                                    <span>Settings</span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem asChild>
-                                <Link href="/groups/new">
-                                    <Plus className="mr-2 h-4 w-4" />
-                                    <span>New Team</span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem asChild>
-                                <Link href="https://github.com/osorionicolas/moneytalks" target="_blank">
-                                    <Github className="mr-2 h-4 w-4" />
-                                    <span>GitHub</span>
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem onClick={() => signOut()}>
-                                <LogOut className="mr-2 h-4 w-4" />
-                                <span>Log out</span>
-                            </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            ) : (
-                <Button onClick={() => signIn()} variant="ghost">
-                    <LogIn className="mr-2 h-4 w-4" />
-                    <span>Log in</span>
-                </Button>
-            )}
-        </header>
-    )
+  const { data: session } = useSession()
+  return (
+      <header className="flex w-full justify-center px-4 py-2 bg-green-500 text-white gap-80">
+          <div className="flex justify-center items-center basis-1/3">
+              <Link href="/" className="rounded-md">
+                  <div className="text-xl font-bold cursor-pointer px-4 py-2 opacity-80 hover:opacity-100">Moneytalks</div>
+              </Link>
+          </div>
+          <div className="flex justify-center items-center basis-1/3">
+              {session ? (
+                  <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" className="text-md">
+                              {session?.user?.name}
+                              <ChevronDown size={16} />
+                          </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuGroup>
+                              <DropdownMenuItem asChild>
+                                  <Link href="/account/settings">
+                                      <div className="flex items-center p-1 cursor-pointer">
+                                          <Settings className="mr-2 h-4 w-4" />
+                                          Settings
+                                      </div>
+                                  </Link>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem asChild>
+                                  <Link href="/groups/new">
+                                      <div className="flex items-center p-1 cursor-pointer">
+                                          <Plus className="mr-2 h-4 w-4" />
+                                          New Team
+                                      </div>
+                                  </Link>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem asChild>
+                                  <Link href="https://github.com/osorionicolas/moneytalks" target="_blank">
+                                      <div className="flex items-center p-1 cursor-pointer">
+                                          <Github className="mr-2 h-4 w-4" />
+                                          GitHub
+                                      </div>
+                                  </Link>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem onClick={() => signOut()}>
+                                  <div className="flex items-center p-1 cursor-pointer">
+                                      <LogOut className="mr-2 h-4 w-4" />
+                                      Log out
+                                  </div>
+                              </DropdownMenuItem>
+                          </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                  </DropdownMenu>
+              ) : (
+                  <Button onClick={() => signIn()} variant="ghost">
+                      <LogIn className="mr-2 h-4 w-4" />
+                      Log in
+                  </Button>
+              )}
+          </div>
+      </header>
+  )
 }
